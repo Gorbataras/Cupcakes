d7a2ecb878224cbf112e7bda501f1d0f37aa522d
Gorbataras <gorbataras@gmail.com>
2020-01-10 14:13:02 -0500

    Form boilerplate

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,19 +17,33 @@ error_reporting(E_ALL);
 <form action="#" method="post" id="cupcakeForm">
     <h1>Cupcake Fundraiser</h1>
 
+    <label for="name">Please Input Your Name: </label> <br>
     <input type="text" name="name" id="name">
-    <label for="name">Please Input Your Name: </label>
+
 
     <br>
 
     <p>Cupcake Flavors</p>
 
     <?php
-        
 
+    $cupcakes = array(
+            "grasshopper"=>'The Grasshopper',
+            'maple'=>'Whiskey Maple Bacon',
+            'carrot'=> 'Carrot Walnut',
+            'caramel'=>'Salted Caramel Cupcake',
+            'velvet'=>'Red Velvet',
+            'lemon'=> 'Lemon Drop',
+            'tiramisu' => 'Tiramisu');
+
+    foreach ($cupcakes as $key => $value) {
+        echo "<input type='checkbox' name='$key' value='$value'> $value <br>";
+    }
+
+    echo "<br><button id='order' name='order'>Order</button>";
 
     ?>
-
+    
 
 </form>
 
