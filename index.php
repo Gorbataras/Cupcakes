23a835a7d4dfd8905f6400da41c51a4525fa3539
Gorbataras <gorbataras@gmail.com>
2020-01-10 13:11:56 -0500

    Initial commit: Main form

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcake Fundraiser</title>
+</head>
+<body>
+<form action="#" method="post" id="cupcakeForm">
+    <h1>Cupcake Fundraiser</h1>
+
+    <input type="text" name="name" id="name">
+    <label for="name">Please Input Your Name: </label>
+
+    <br>
+
+    <p>Cupcake Flavors</p>
+
+    <?php
+        
+
+
+    ?>
+
+
+</form>
+
+</body>
+</html>
