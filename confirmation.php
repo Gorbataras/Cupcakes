6da20bae11bbb3d80e69c36b828104ee31ceee62
Gorbataras <gorbataras@gmail.com>
2020-01-13 16:15:41 -0500

    Printing order summary and total

--- confirmation.php
+++ confirmation.php
@@ -15,17 +15,55 @@ error_reporting(E_ALL);
 <body>
 <?php
 
+$isValid = true;
+
 //Validating name
 if (!empty($_POST['name'])) {
     $first = trim($_POST['name']);
+    echo "Thank you, ".$first.", for your order!";
 } else {
     echo '<p>Please enter name.</p>';
     $isValid = false;
+    echo"<p><a href='./index.php'>GO Back to order Form.</a></p>";
 }
 
+$cupcakes = array(
+    "grasshopper"=>'The Grasshopper',
+    'maple'=>'Whiskey Maple Bacon',
+    'carrot'=> 'Carrot Walnut',
+    'caramel'=>'Salted Caramel Cupcake',
+    'velvet'=>'Red Velvet',
+    'lemon'=> 'Lemon Drop',
+    'tiramisu' => 'Tiramisu');
+
+$selectedCupcakes = array();
+
 foreach ($cupcakes as $key => $value) {
-    echo $key.$value;
+    if (isset($_POST[$key])) {
+        array_push($selectedCupcakes, $value);
+
+    }
 }
+if (empty($selectedCupcakes)) {
+    echo '<p>You did not select any cupcakes.</p>';
+    $isValid = false;
+    echo"<p><a href='./index.php'>GO Back to order Form.</a></p>";
+} else {
+    echo "<p>Order Summary: </p>";
+    echo "<ul>";
+    foreach ($selectedCupcakes as $value) {
+        echo "<li>$value</li>";
+    }
+    echo "</ul>";
+
+    //Total Calculation
+    $total = count($selectedCupcakes) * 3.5;
+
+    echo "<p>Order Total: $".$total."</p>";
+
+
+}
+
 
 ?>
 </body>
